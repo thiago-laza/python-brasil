54c0ef4239b7fec93b55a9afeea9c9fbb1298024
thiago-laza <thiago.profmat@gmail.com>
2023-11-18 10:54:25 -0300

    Work1: inicio est rep

--- 3-estrutura-de-repeticao/ex1.py
+++ 3-estrutura-de-repeticao/ex1.py
@@ -1,2 +1,9 @@
 '''Faça um programa que peça uma nota, entre zero e dez. Mostre uma mensagem caso o valor seja inválido e continue 
 pedindo até que o usuário informe um valor válido.'''
+
+nota = float(input('Digite sua nota: '))
+
+while nota < 0 or nota > 10:
+    nota =float(input('Digite uma nota valida (entre zero e dez): '))
+
+print(f'{nota} e uma nota valida.')
