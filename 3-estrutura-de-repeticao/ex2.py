0227780a92ed75491d8348722bb2c418287f7e5a
thiago-laza <thiago.profmat@gmail.com>
2023-11-19 10:29:07 -0300

    Home: inicio estr rep

--- 3-estrutura-de-repeticao/ex2.py
+++ 3-estrutura-de-repeticao/ex2.py
@@ -1,2 +1,11 @@
 '''Faça um programa que leia um nome de usuário e a sua senha e não aceite a senha igual ao nome do usuário, 
 mostrando uma mensagem de erro e voltando a pedir as informações.'''
+
+nome = input('Usuario: ')
+senha = input('Senha: ')
+
+while nome == senha:
+    print('Erro!')
+    nome = input('Usuario: ')
+    senha = input('Senha: ')
+    print('Usuario logado.')
--- 3-estrutura-de-repeticao/ex4.py
+++ 3-estrutura-de-repeticao/ex4.py
@@ -2,3 +2,24 @@
 e que a população de B seja 200000 habitantes com uma taxa de crescimento de 1.5%. Faça um programa que calcule 
 e escreva o número de anos necessários para que a população do país A ultrapasse ou iguale a população do país B, 
 mantidas as taxas de crescimento.'''
+
+t = 0
+
+populacao_a = 80000
+taxa_a = 1.03
+
+populacao_b = 200000
+taxa_b = 1.015
+
+while populacao_a <= populacao_b:
+    populacao_a *= taxa_a
+    populacao_b *= taxa_b
+    t+=1
+    print(t,populacao_a,populacao_b)
+
+print(f'A populacao do pais A vai ultrapassar a populacao do pais B em {t} anos.')
+    
+
+
+
+
